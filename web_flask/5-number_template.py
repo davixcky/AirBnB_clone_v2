a90dd11db2e4217d6e819ba8bea53d5330a6efd2
Orozco David <dvdizcky@gmail.com>
2020-09-02 18:42:17 -0500

    Import fixed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """
 Starts a Flask web application
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -44,6 +44,7 @@ def is_numeber(n):
 def numbersandtemplates(n):
     """Display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
